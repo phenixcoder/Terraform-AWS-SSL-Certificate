7d3840365828df7d02c2d6d0ed9920a7883e2c9e
Balwant <balwant.matharu@gmail.com>
2023-06-11 00:03:51 +1000

    Added required providers block

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.2.0"
+    }
+  }
+}
 data "aws_route53_zone" "hosting_zone" {
   name         = var.hosting_zone_domain_name == "" ? var.fqdn : var.hosting_zone_domain_name
   private_zone = !var.hosting_zone_public
